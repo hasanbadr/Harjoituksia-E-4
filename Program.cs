8d1f2a4092e6d7458c0ed40c997ca15f9477a0cd
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 08:04:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Syötä ensimmäinen kokonaisluku: ");
+        int luku1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Syötä toinen kokonaisluku: ");
+        int luku2 = int.Parse(Console.ReadLine());
+
+        if (luku1 > 0 && luku2 > 0)
+        {
+            Console.WriteLine("Molemmat ovat positiivisia.");
+        }
+        else if (luku1 < 0 && luku2 < 0)
+        {
+            Console.WriteLine("Molemmat ovat negatiivisia.");
+        }
+        else
+        {
+            Console.WriteLine("Toinen on positiivinen ja toinen negatiivinen.");
+        }
+    }
+}
